a2d795534bbbc276c83627b999dba503e58233eb
hadd <cristianp4lt4@gmail.com>
2023-04-11 08:19:10 -0500

    creacion tabla producto

---
+++ Ejercicio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE cliente {
+    id_cliente INT PRIMARY KEY, 
+    nombre VARCHAR (50),
+    edad INT (10)
+}
+CREATE TABLA producto {
+    id_producto INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    fecha_produccion VARCHAR (50)
+}
